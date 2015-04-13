dafae350d9212db59808e36dc5e34ccce6c64e50
Than Dang <dtthan@apide.com>
2015-04-13 06:49:58 +0200

    initial project

---
+++ TComponents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TComponents
+//
+//  Created by Dang Thanh Than on 4/13/15.
+//  Copyright (c) 2015 Than Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
